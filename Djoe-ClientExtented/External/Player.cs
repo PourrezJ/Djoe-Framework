bca2925aca0bd23973181dfda24db9ce935dca53
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2020-07-27 08:15:31 +0200

    Player oop update

--- Djoe-ClientExtented/External/Player.cs
+++ Djoe-ClientExtented/External/Player.cs
@@ -33,6 +33,73 @@ namespace ClientExtended.External
         }
 
         public string Name => Function.Call<string>(Hash.GET_PLAYER_NAME, Handle);
+
+        public static int Money
+        {
+            get => Function.Call<int>((Hash)0x0C02DABFA3B98176);
+            set
+            {
+                var source = Money;
+                var target = value;
+                if (target < source)
+                {
+                    Function.Call((Hash)0x466BC8769CF26A7A, source - target);
+                }
+                else
+                {
+                    Function.Call((Hash)0xBC3422DC91667621, target - source);
+                }
+            }
+        }
+
+        public int WantedLevel
+        {
+            get => Function.Call<int>(Hash.GET_PLAYER_WANTED_LEVEL, Handle);
+            set
+            {
+                Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Handle, value, false);
+            }
+        }
+
+        public bool IsDead => Function.Call<bool>(Hash.IS_PLAYER_DEAD, Handle);
+
+        public bool IsAlive => !IsDead;
+
+        public bool IsAiming => Function.Call<bool>(Hash.IS_PLAYER_FREE_AIMING, Handle);
+
+        public bool IsClimbing => Function.Call<bool>(Hash.IS_PLAYER_CLIMBING, Handle);
+
+        public bool IsRidingTrain => Function.Call<bool>(Hash.IS_PLAYER_RIDING_TRAIN, Handle);
+
+
+        public bool IsPlaying => Function.Call<bool>(Hash.IS_PLAYER_PLAYING, Handle);
+
+        public bool IsInvincible
+        {
+            get => Function.Call<bool>(Hash.GET_PLAYER_INVINCIBLE, Handle);
+            set => Function.Call(Hash.SET_PLAYER_INVINCIBLE, Handle, value);
+        }
+
+        public bool IgnoredByEveryone
+        {
+            set => Function.Call(Hash.SET_EVERYONE_IGNORE_PLAYER, Handle, value);
+        }
+
+        public bool CanUseCover
+        {
+            set => Function.Call(Hash.SET_PLAYER_CAN_USE_COVER, Handle, value);
+        }
+
+        public bool CanStartMission
+        {
+            get => Function.Call<bool>(Hash.CAN_PLAYER_START_MISSION, Handle);
+        }
+
+        public bool CanControlCharacter
+        {
+            get => Function.Call<bool>(Hash.IS_PLAYER_CONTROL_ON, Handle);
+            set => Function.Call(Hash.SET_PLAYER_CONTROL, Handle, value, 0, 0);
+        }
         public int ServerId => Function.Call<int>(Hash.GET_PLAYER_SERVER_ID, Handle);
 
         public async Task<bool> ChangeModel(Model model, int timeout = 3000)
@@ -48,6 +115,38 @@ namespace ClientExtended.External
 
             return true;
         }
+
+
+        public Vehicle LastVehicle => Function.Call<Vehicle>(Hash.GET_PLAYERS_LAST_VEHICLE);
+
+        public bool IsTargetting(Entity entity)
+        {
+            return Function.Call<bool>(Hash.IS_PLAYER_FREE_AIMING_AT_ENTITY, Handle, entity.Handle, 0);
+        }
+
+        public bool IsTargettingAnything => Function.Call<bool>(Hash.IS_PLAYER_TARGETTING_ANYTHING, Handle);
+
+
+        public void DisableFiringThisFrame(bool toggle)
+        {
+            Function.Call(Hash.DISABLE_PLAYER_FIRING, Handle, toggle);
+        }
+
+        public void SetSuperJumpThisFrame()
+        {
+            Function.Call(Hash.SET_SUPER_JUMP_THIS_FRAME, Handle);
+        }
+
+        public void SetMayNotEnterAnyVehicleThisFrame()
+        {
+            Function.Call(Hash.SET_PLAYER_MAY_NOT_ENTER_ANY_VEHICLE, Handle);
+        }
+
+        public void SetMayOnlyEnterThisVehicleThisFrame(Vehicle vehicle)
+        {
+            Function.Call(Hash.SET_PLAYER_MAY_ONLY_ENTER_THIS_VEHICLE, Handle, vehicle);
+        }
+
 
         public bool Equals(Player other)
         {

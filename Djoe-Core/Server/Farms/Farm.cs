af7831598556b89a4293380d306be8a9e026bf81
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2020-08-09 09:16:00 +0200

    Farm begin implantation

---
+++ Djoe-Core/Server/Farms/Farm.cs
@@ -0,0 +1,43 @@
+﻿using CitizenFX.Core;
+using Server.Controllers;
+using Server.Utils;
+using Server.Utils.Enums;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Farms
+{
+    public enum FarmType
+    {
+        Unknown = 0,
+        Miner = 1
+    }
+
+    public abstract class Farm
+    {
+        public FarmType FarmType;
+
+        public string FarmName;
+        public Vector3 FarmBlipPos;
+        public BlipSprite FarmBlipSprite;
+        public UCoords[] FarmPos;
+
+
+
+        public virtual void Init()
+        {
+            if (FarmBlipPos != null)
+            {
+                BlipsManager.CreateBlip(FarmName, FarmBlipSprite, BlipType.WeaponWhite, FarmBlipPos, 0);
+            }
+
+            Logger.Debug($"Farm {FarmName} initialized.");
+        }
+
+        public abstract void OnFarm(Player player);
+    }
+}
---
+++ Djoe-Core/Server/Farms/Miner.cs
@@ -0,0 +1,31 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Farms
+{
+
+    public class Miner : Farm
+    {
+        public Miner(string name, Vector3 blipPos) : base(name, blipPos)
+        {
+        }
+
+        public override void Init()
+        {
+
+
+            base.Init();
+        }
+
+        public override void OnFarm(Player player)
+        {
+            
+            
+  
+        }
+    }
+}
